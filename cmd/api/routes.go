1962cf59dae72f63e8c5469868351c01cf8ae440
shuvoedward <38678545+shuvoedward@users.noreply.github.com>
2025-09-02 19:21:14 -0400

    feat: Add basic router functionality

---
+++ cmd/api/routes.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) routes() http.Handler {
+	router := httprouter.New()
+	return router
+}
