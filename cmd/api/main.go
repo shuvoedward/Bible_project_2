eb279f537d853b7926005834a355e85eff101fd9
shuvoedward <38678545+shuvoedward@users.noreply.github.com>
2025-09-03 03:28:50 -0400

    feat: Add openDB function to establish database connection
    This commit introduces a new `openDB` function that handles the database connection using a DSN from an environment variable. This function centralizes the connection logic and includes a
    `Ping` check to ensure the connection is successful.
    
    It uses the `database/sql` package to return a `*sql.DB` object,
    which can be passed to other parts of the application for database access.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,25 +1,51 @@
 package main
 
 import (
+	"database/sql"
 	"log/slog"
 	"os"
+
+	_ "github.com/lib/pq"
 )
 
 type application struct {
 	logger *slog.Logger
+	db     *sql.DB
 }
 
 func main() {
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
-	app := application{
-		logger: logger,
-	}
-
-	err := app.serve()
+	BIBLE_DB_DSN := os.Getenv("BIBLE_DB_DSN")
+	db, err := openDB(BIBLE_DB_DSN)
 	if err != nil {
 		logger.Error(err.Error())
 		os.Exit(1)
 	}
 
+	app := application{
+		logger: logger,
+	}
+
+	err = app.serve()
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
