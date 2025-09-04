3c115669d2b371d815a11f2523c451daa1a945dc
shuvoedward <38678545+shuvoedward@users.noreply.github.com>
2025-09-04 02:23:02 -0400

    feat: Implement model for fetching Bible passages

--- internal/data/books.go
+++ internal/data/books.go
@@ -28,22 +28,72 @@ func NewPassageModel(db *sql.DB) PassageModel {
 }
 
 func (p *PassageModel) Get(filters PassageFilters) (*Passage, error) {
-	query := `
-        SELECT v.verse, v.text
-        FROM verses as v
-        JOIN books As b ON v.book_id = b.id
-        WHERE b.name = $1 AND v.chapter = $2`
-
-	args := []any{filters.Book, filters.Chapter}
 
 	if filters.Verse != 0 {
-		query += " AND v.verse = $3"
-		args = append(args, filters.Verse)
+		return p.getSingleVerse(filters)
 	} else if filters.StartVerse != 0 && filters.EndVerse != 0 {
-		query += " AND v.verse BETWEEN $3 AND $4"
-		args = append(args, filters.StartVerse, filters.EndVerse)
+		return p.getVerseRange(filters)
+	} else {
+		return p.getChapter(filters)
+	}
+}
+
+func (p *PassageModel) getSingleVerse(filters PassageFilters) (*Passage, error) {
+	query := `
+		SELECT v.verse, v.text
+		FROM verses as v
+		JOIN books as b ON v.book_id = b.id
+		WHERE b.name = $1 AND v.chapter = $2 AND v.verse = $3`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	args := []any{filters.Book, filters.Chapter, filters.Verse}
+
+	var verseDetail VerseDetail
+
+	err := p.DB.QueryRowContext(ctx, query, args...).Scan(
+		&verseDetail.Number,
+		&verseDetail.Text,
+	)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
 	}
 
+	return &Passage{
+		Book:    filters.Book,
+		Chapter: filters.Chapter,
+		Verses:  []VerseDetail{verseDetail},
+	}, nil
+
+}
+
+func (p *PassageModel) getVerseRange(filters PassageFilters) (*Passage, error) {
+	query := `
+			SELECT  v.verse, v.text
+			FROM verses as v
+			JOIN books AS b ON b.id = v.book_id
+			WHERE b.name = $1 AND v.chapter = $2 AND v.verse BETWEEN $3 AND $4`
+
+	return p.queryVerses(query, filters.Book, filters.Chapter, filters.StartVerse, filters.EndVerse)
+}
+
+func (p *PassageModel) getChapter(filters PassageFilters) (*Passage, error) {
+	query := `
+			SELECT v.verse, v.text
+			FROM verses as v
+			JOIN books As b ON v.book_id = b.id
+			WHERE b.name = $1 AND v.chapter = $2`
+	return p.queryVerses(query, filters.Book, filters.Chapter)
+}
+
+func (p *PassageModel) queryVerses(query string, args ...any) (*Passage, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
@@ -53,19 +103,19 @@ func (p *PassageModel) Get(filters PassageFilters) (*Passage, error) {
 	}
 	defer rows.Close()
 
-	passage := Passage{
-		Book:    filters.Book,
-		Chapter: filters.Chapter,
+	passage := &Passage{
+		Book:    args[0].(string),
+		Chapter: args[1].(int),
+		Verses:  []VerseDetail{},
 	}
 
 	for rows.Next() {
 		var verseDetail VerseDetail
-		if err := rows.Scan(&verseDetail.Number, &verseDetail.Text); err != nil {
+		if err := rows.Scan(&verseDetail.Number, verseDetail.Text); err != nil {
 			return nil, err
 		}
 		passage.Verses = append(passage.Verses, verseDetail)
 	}
-
 	if err = rows.Err(); err != nil {
 		return nil, err
 	}
@@ -74,5 +124,5 @@ func (p *PassageModel) Get(filters PassageFilters) (*Passage, error) {
 		return nil, ErrRecordNotFound
 	}
 
-	return &passage, nil
+	return passage, nil
 }
