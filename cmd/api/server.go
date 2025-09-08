6dce3e9eb3f9f60e9c2ed1e06f09bbfcb36c0ea7
shuvoedward <38678545+shuvoedward@users.noreply.github.com>
2025-09-08 18:20:22 -0400

    feat: Add graceful server shutdown functionality
    Introduce a graceful shutdown routine for the HTTP server to handle SIGINT and SIGTERM signals. This ensures that the server can finish processing active requests before exiting, preventing abrupt termination and potential data loss. A 30-second timeout is included for in-flight requests.

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 func (app *application) serve() error {
 	srv := &http.Server{
-		Addr:         ":8080",
+		Addr:         ":4000",
 		Handler:      app.routes(),
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
@@ -16,11 +21,37 @@ func (app *application) serve() error {
 		ErrorLog:     slog.NewLogLogger(app.logger.Handler(), slog.LevelError),
 	}
 
-	err := srv.ListenAndServe()
+	shutdownError := make(chan error)
 
+	go func() {
+		quit := make(chan os.Signal, 1)
+
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		s := <-quit
+
+		app.logger.Info("shutting down server", "signal", s.String())
+
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		err := srv.Shutdown(ctx)
+		if err != nil {
+			shutdownError <- err
+		}
+	}()
+
+	app.logger.Info("Started server...")
+
+	err := srv.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	err = <-shutdownError
 	if err != nil {
 		return err
 	}
-	return nil
 
+	app.logger.Info("stopped server", "addr", srv.Addr)
+	return nil
 }
